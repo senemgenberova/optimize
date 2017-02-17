1554107fbfb31291778387ea5526b97795928890
senemgenberova <senemgenberova@gmail.com>
2017-02-17 02:46:59 -0800

    Update productCodeByOrderId.sql

--- productCodeByOrderId.sql
+++ productCodeByOrderId.sql
@@ -1,4 +1,4 @@
-SELECT pr.productCode 
+SELECT pr.productCode , pr.productName
 FROM `orderdetails` od
 INNER JOIN orders ord ON od.orderNumber = ord.orderNumber
 INNER JOIN products pr ON od.productCode = pr.productCode
